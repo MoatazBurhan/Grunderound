c988e6e4af5fa29edafd65088c642307eda41ea1
Moataz Burhan <moataz@moatazs-macbook.attlocal.net>
2021-02-14 23:00:52 -0600

    file structure

--- controller/src/go/main.go
+++ controller/src/go/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "redirecting to link based on %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "url %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
